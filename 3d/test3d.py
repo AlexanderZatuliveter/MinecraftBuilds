d47bef113485068d8955cdf149ba6c61bf04322b
Vladimir Zatuliveter <vzatuliveter@actsoft.com>
2025-06-14 10:48:28 +0600

    requirements.txt added.

--- 3d/test3d.py
+++ 3d/test3d.py
@@ -1,12 +1,12 @@
 from mcpq import Minecraft, Vec3
 import trimesh
 
-mesh = trimesh.load("C:\Data\Code\Personal\Minecraft\\3d\\glad3Dprintable-animal-octopus.STL")
+mesh = trimesh.load("C:\\Data\\Code\\Personal\\Minecraft\\3d\\glad3Dprintable-animal-octopus.STL")
 print('loaded')
 
 mesh.apply_scale(0.025)
 
-voxelized = mesh.voxelized(pitch=1.0) 
+voxelized = mesh.voxelized(pitch=1.0)
 print('voxelized')
 
 voxels = voxelized.points.astype(int)
@@ -21,4 +21,3 @@ start = Vec3(1567, 50, -791)  # origin of world, coordinates x, y and z = 0
 blocks = [start + Vec3(voxel[0], voxel[2], voxel[1]) for voxel in voxels]
 
 mc.setBlockList("stone", blocks)
-    
