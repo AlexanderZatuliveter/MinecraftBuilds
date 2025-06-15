502d65cf31dd1ee1368044d22f8fe348186984f9
Vladimir Zatuliveter <vzatuliveter@actsoft.com>
2025-06-15 11:33:55 +0600

    3d printing stuff.

--- 3d/test3d.py
+++ 3d/test3d.py
@@ -2,11 +2,12 @@ from mcpq import Minecraft, Vec3
 import trimesh
 from common.files import get_resource_path
 
-file = get_resource_path("3d\\models\\glad3Dprintable-animal-octopus.STL")
+file_name = "Trex.stl"
+file = get_resource_path(f"3d\\models\\{file_name}")
 mesh = trimesh.load(file)
 print('loaded')
 
-mesh.apply_scale(0.025)
+mesh.apply_scale(3.5)
 
 voxelized = mesh.voxelized(pitch=1.0)
 print('voxelized')
@@ -18,7 +19,7 @@ mc = Minecraft('192.168.1.77')  # connect to server on localhost
 
 mc.postToChat("Printing!")
 
-start = Vec3(1567, 50, -791)  # origin of world, coordinates x, y and z = 0
+start = Vec3(550, 30, 1400)  # origin of world, coordinates x, y and z = 0
 
 blocks = [start + Vec3(voxel[0], voxel[2], voxel[1]) for voxel in voxels]
 
